6933ca2154d01cc7dd6c651aafe05ec5d066d375
Mahmoud Feraas <mahmoudferaas7@gmail.com>
2021-04-15 23:26:43 +0200

    add reservation unit tests

---
+++ BackEnd/Brightskies.Restaurant/Test/Brightskies.Restaurant.Application.UnitTest/Reservations/Commands/Create/CreateReservationCommandHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using Brightskies.Restaurant.Application.Reservations.Commands.Create;
+using Brightskies.Restaurant.Application.UnitTest.Common;
+using Brightskies.Restaurant.Domain.Entities;
+using Moq;
+using Shouldly;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Brightskies.Restaurant.Application.UnitTest.Reservations.Commands.Create
+{
+    public class CreateReservationCommandHandlerTests : CommandTestBase
+    {
+        [Theory]
+        [NoRecursion]
+        public async Task Handle_ValidCommand_ShouldSaveEntriesSuccessfully(Reservation Reservation)
+        {
+            // Arrange
+            _mapperMock.Setup(m => m.Map<Reservation>(It.IsAny<CreateReservationCommand>())).Returns(Reservation); 
+
+            var sut = new CreateReservationCommandHandler(_context, _mapperMock.Object); // creating system under test
+
+            // Act
+            await sut.Handle(new CreateReservationCommand(), CancellationToken.None);
+
+            // Assert
+            _context.Reservations.Count().ShouldBe(1);
+        }
+    }
+}
---
+++ BackEnd/Brightskies.Restaurant/Test/Brightskies.Restaurant.Application.UnitTest/Reservations/Queries/GetAll/GetAllReservationQueryTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoFixture;
+using Brightskies.Restaurant.Application.Reservations.Dtos;
+using Brightskies.Restaurant.Application.Reservations.Queries.GetAll;
+using Brightskies.Restaurant.Application.UnitTest.Common;
+using Brightskies.Restaurant.Domain.Entities;
+using Moq;
+using Shouldly;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Brightskies.Restaurant.Application.UnitTest.Reservations.Queries.GetAll
+{
+    public class GetAllReservationQueryTests : CommandTestBase
+    {
+        private readonly IFixture _fixture;
+
+        public GetAllReservationQueryTests()
+        {
+            _fixture = new Fixture();
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        }
+
+        [Theory]
+        [NoRecursion]
+        public async Task Handle_GetAllQuery_ShouldReturnEntriesSuccessfully(List<ReservationDto> output)
+        {
+            // Arrange
+            _mapperMock.Setup(m => m.Map<List<ReservationDto>>(It.IsAny<List<Reservation>>())).Returns(output);
+
+            var sut = new GetAllReservationQueryHandler(_context, _mapperMock.Object);
+
+            var temUser = _fixture.Create<Reservation>();
+
+            // Act
+            await ContextOperation.CreateEntity(_context, temUser);
+
+            var result = await sut.Handle(new GetAllReservationQuery(), CancellationToken.None);
+
+            // Assert
+            result.Count().ShouldBeGreaterThan(0);
+        }
+    }
+}
